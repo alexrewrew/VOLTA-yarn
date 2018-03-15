f91550247b7414139ed610b36a68a8d4a9d69106
alexrewrew <alexrewrew@live.com>
2018-03-15 16:47:16 +0200

    Update 15.03

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -2,9 +2,6 @@ module.exports = function () {
     $.gulp.task('scripts:lib', function () {
         return $.gulp.src([
             'node_modules/jquery/dist/jquery.js',
-            // 'node_modules/jquery-ui-dist/jquery-ui.js',
-            // 'node_modules/jquery-ui/ui/i18n/datepicker-uk.js',
-            // 'node_modules/jquery-ui/ui/i18n/datepicker-ru.js',
             // 'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
             // 'app/source/fontawesome-free-5.0.6/svg-with-js/js/fontawesome-all.js',
             'node_modules/chosen-js/chosen.jquery.js',
